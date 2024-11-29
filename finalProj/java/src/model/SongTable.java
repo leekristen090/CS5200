c4626ad949a32e23a0fa0c8233cc2f4d831f5955
Kristen Lee <leekristen090@gmail.com>
2024-11-28 23:17:54 -0500

    Add files via upload

---
+++ finalProj/java/src/model/SongTable.java
@@ -0,0 +1,47 @@
+package model;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+public class SongTable {
+
+  public static JTable getSongTable(Connection connection) {
+    JTable table = new JTable();
+    try (Statement statement = connection.createStatement()) {
+      String query = "SELECT * FROM song";
+      ResultSet resultSet = statement.executeQuery(query);
+
+      // Get column count
+      int columnCount = resultSet.getMetaData().getColumnCount();
+      String[] columnNames = new String[columnCount];
+      for (int i = 0; i < columnCount; i++) {
+        columnNames[i] = resultSet.getMetaData().getColumnName(i + 1);
+      }
+
+      // Create the table model
+      DefaultTableModel model = new DefaultTableModel();
+      model.setColumnIdentifiers(columnNames);
+
+      // Add data to table
+      while (resultSet.next()) {
+        Object[] row = new Object[columnCount];
+        for (int i = 0; i < columnCount; i++) {
+          row[i] = resultSet.getObject(i + 1);
+        }
+        model.addRow(row);
+      }
+
+      table.setModel(model);
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+      JOptionPane.showMessageDialog(null, "Error loading song data: "
+              + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+    }
+    return table;
+  }
+}
