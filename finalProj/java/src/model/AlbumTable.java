3dd7816700161275020e8e99ba3996d81aeba611
Kristen Lee <leekristen090@gmail.com>
2024-11-29 23:18:07 -0500

    Add files via upload

--- finalProj/java/src/model/AlbumTable.java
+++ finalProj/java/src/model/AlbumTable.java
@@ -1,7 +1,10 @@
 package model;
 
 import javax.swing.JTable;
+
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class AlbumTable {
 
@@ -9,4 +12,19 @@ public class AlbumTable {
     String query = "SELECT * FROM album";
     return TableUtil.fetchTableData(connection, query);
   }
+
+  public static boolean addTupleWithProcedure(Connection connection, String albumName,
+                                              String releaseDate) {
+    String call = "{CALL addAlbumTuple(?, ?)}";
+    try (CallableStatement callableStatement = connection.prepareCall(call)) {
+      callableStatement.setString(1, albumName);
+      callableStatement.setString(2, releaseDate);
+      int rowsAffected = callableStatement.executeUpdate();
+      return rowsAffected > 0;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
+
 }
