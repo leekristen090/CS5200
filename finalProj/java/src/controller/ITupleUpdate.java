9a0c601e91470a503fc8fd7465f8e65671db32ea
Kristen Lee <leekristen090@gmail.com>
2024-12-03 23:29:04 -0500

    Add files via upload

--- finalProj/java/src/controller/ITupleUpdate.java
+++ finalProj/java/src/controller/ITupleUpdate.java
@@ -1,3 +1,22 @@
 package controller;
 
-public interface ITupleUpdate {}
+import java.sql.Connection;
+
+import javax.swing.*;
+
+/**
+ * This is the tuple update interface which has the method for updating a specified tuple
+ * from a chosen table in the team_sabrina database.
+ */
+public interface ITupleUpdate {
+
+  /**
+   * Update a tuple in the selected db table with the corresponding user input from the
+   * input panel.
+   * @param tableName selected table
+   * @param inputPanel user input panel
+   * @param connection db connection
+   * @return true if tuple updated successfully, false otherwise
+   */
+  boolean updateTuple(String tableName, JPanel inputPanel, Connection connection);
+}
