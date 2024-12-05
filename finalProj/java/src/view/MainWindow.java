3ff6d7377ce6e41e45847a54603e9d50b96da5fe
Kristen Lee <leekristen090@gmail.com>
2024-12-05 00:28:22 -0500

    Add files via upload

--- finalProj/java/src/view/MainWindow.java
+++ finalProj/java/src/view/MainWindow.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 import java.awt.*;
-import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
